f01e3db08a5a492daeaf53a233bc91b376aff850
John Anderson <sontek@gmail.com>
2022-02-20 00:22:40 -0400

    put path on the prompt

--- dotfiles/zsh/.config/zsh/prompt.zsh
+++ dotfiles/zsh/.config/zsh/prompt.zsh
@@ -1,8 +1,9 @@
+# man zshmisc for docs
+
 # Load colors so we can access $fg and more.
 autoload -U colors && colors
 
-# %B / %b = start/stop bold
-#
+
 git_prompt() {
     local branch="$(git symbolic-ref HEAD 2> /dev/null | cut -d'/' -f3)"
     local branch_truncated="${branch:0:30}"
@@ -18,5 +19,9 @@ setopt PROMPT_SUBST
 
 # The %{ %} around this is important.  Otherwise big gaps
 # will appear when doing things like reverse search
-PROMPT='%{$fg[green]%}$(git_prompt)%(?.%{$fg[blue]%}.%{$fg[red]%}) %B❯%b '
+
+# %B / %b = start/stop bold
+# %~ = path releative to $HOME
+NL=$'\n'
+PROMPT='%{$fg[yellow]%}%~%{$fg[green]%}$(git_prompt)%(?.%{$fg[blue]%}.%{$fg[red]%})$NL%B❯%b '
 
