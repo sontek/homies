2b644f2a39e0a10050c728d958c826b779e30fa3
John Anderson <sontek@gmail.com>
2022-06-12 20:36:38 -0400

    fix completions

--- dotfiles/nvim/.config/nvim/lua/sontek/completion.lua
+++ dotfiles/nvim/.config/nvim/lua/sontek/completion.lua
@@ -2,6 +2,15 @@ local lspkind = require('lspkind')
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
     sources = {
         { name = "nvim_lsp" },
